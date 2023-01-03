fb411ab162344ff2dbc94f30a2cafbe1e52e6053
S. Str <45916848+sebk26@users.noreply.github.com>
2023-01-03 18:43:09 +0100

    Add Multi-Process per GPU minimal example

---
+++ MultiProcPerGPU_DPP.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --time=0-01:00:00
+#SBATCH -J TestDDP_GPUBind_MPIDatloader
+#SBATCH --output=Test-R-%j-%x.log
+#SBATCH --gres=gpu:8
+#SBATCH -p alpha,alpha-interactive
+#SBATCH --mem=0
+#SBATCH -N 3
+#SBATCH --ntasks-per-node=24
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus-per-node=8
+#SBATCH --gpu-bind=single:3
+#SBATCH --constraint=fs_beegfs
+#SBATCH --hint=nomultithread
+## 
+module purge
+ml modenv/hiera 
+ml GCC/11.3.0
+ml OpenMPI/4.1.4
+ml imkl/2022.0.1
+ml CUDA/11.7.0
+ml cuDNN/8.4.1.50-CUDA-11.7.0
+ml NCCL/2.12.12-CUDA-11.7.0
+ml Python/3.9.6-bare
+source /beegfs/.global0/ws/sekr289a-ML2-n/virtualenvs/py3.9.6_cu117_imkl_torch1.13/bin/activate
+
+srun --distribution=plane=3 python3 -u ./src/main_MPIDatloader.py
---
+++ MultiProcPerGPU_DPPMPI.py
@@ -0,0 +1,68 @@
+import os, sys
+import hostlist  #pip install python-hostlist
+from mpi4py import MPI
+from time import sleep
+
+if __name__ == '__main__':
+
+    comm_world = MPI.COMM_WORLD
+    worldrank = comm_world.Get_rank()
+    worldsize = comm_world.Get_size()
+
+    assert worldrank == int(os.environ['SLURM_PROCID']) #check for multinode
+    local_rank = int(os.environ['SLURM_LOCALID'])
+    size = int(os.environ['SLURM_NTASKS'])
+    cpus_per_task = int(os.environ['SLURM_CPUS_PER_TASK'])
+    local_nodeID = int(os.environ["SLURM_NODEID"])
+    # get node list from slurm
+    hostnames = hostlist.expand_hostlist(os.environ['SLURM_JOB_NODELIST'])
+        
+    # get IDs of reserved GPU
+    gpu_ids = os.environ['SLURM_STEP_GPUS'].split(",")
+
+    os.environ['MASTER_ADDR'] = hostnames[0]
+    os.environ['MASTER_PORT'] = str(6512)
+
+    # assert len(gpu_ids) == 1
+    gpu_id = int(gpu_ids[0])
+    sub_comm = comm_world.Split(color=(gpu_id+(local_nodeID)*worldsize))
+    # space_size = space_comm.Get_size()
+    sub_rank = sub_comm.Get_rank()
+    sub_size = sub_comm.Get_size()
+    
+    print(f'{worldrank=} on {local_nodeID=} in Comm with {worldsize=} over {hostnames=}. {gpu_ids=}. {sub_rank=} {sub_size=}')
+
+    sys.stdout.flush()
+
+    if sub_rank == 0:
+        import numpy as np
+        import torch
+        import torch.distributed as dist
+
+        distrank = int(worldrank/3)
+        distsize = int(worldsize/3)
+
+        print(f'{distrank=} {distsize=} {gpu_id=} {os.environ["CUDA_VISIBLE_DEVICES"]=}')
+        #torch.cuda.set_device(gpu_id)
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        torch.cuda.set_device(0)
+        # USES ENVs: MASTER_ADDR and MASTER_PORT that are set in slurmTorch.py
+        dist.init_process_group(backend='nccl',
+                            init_method='env://',
+                            world_size=distsize,
+                            rank=distrank)
+        print(f'{distrank=} : created process group')
+        ranktensor = torch.tensor(distrank, device=device)
+        print(f'{distrank=} {distsize=} {ranktensor=}')
+        dist.all_reduce(ranktensor,op=dist.ReduceOp.SUM)
+        if distrank == 0:
+            print('All_reduced')
+        dist.barrier()
+        print(f'{distrank=} {distsize=} {ranktensor=}')
+        
+        assert ranktensor.cpu().numpy() == np.sum(np.array(range(distsize)))
+        dist.destroy_process_group()
+
+    ### other processes will be used for data preprocessing
+    comm_world.Barrier()
+    
