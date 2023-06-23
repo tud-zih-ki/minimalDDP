dee9faf1c1350f16a03459335d73a586fa602e38
Sebastian Strönisch <45916848+sebk26@users.noreply.github.com>
2023-06-23 11:30:09 +0200

    Update slurmTorch.py
    Use last reserved port in case this conflicts with MPI and provide Fallback

--- slurmTorch.py
+++ slurmTorch.py
@@ -18,7 +18,12 @@ local_nodeID = int(os.environ["SLURM_NODEID"])
 # get node list from slurm
 hostnames = hostlist.expand_hostlist(os.environ['SLURM_JOB_NODELIST'])
     
-# get IDs of reserved GPU
-os.environ['MASTER_ADDR'] = hostnames[0]
-resv_ports = os.environ['SLURM_STEP_RESV_PORTS'].split("-")
-os.environ['MASTER_PORT'] = str(resv_ports[0])
+try:
+    # slurm reserves one port per job
+    resv_ports = os.environ['SLURM_STEP_RESV_PORTS'].split("-")
+    os.environ['MASTER_PORT'] = str(resv_ports[-1])
+    os.environ['MASTER_ADDR'] = hostnames[0]
+except KeyError:
+    print(f'WARNING from slurmTorch.py: Use Fallback MASTER_ADDR and MASTER_PORT')
+    os.environ["MASTER_ADDR"] = "127.0.0.1"
+    os.environ["MASTER_PORT"] = "29500"
